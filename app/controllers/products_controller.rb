efd643dac06440b2bce0b05704facd663bddc351
s1mptom <zikomar@gmail.com>
2012-07-22 04:05:49 +0000

    added quantity in cart to buttons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.paginate(:page => params[:page], per_page: 6)
+    @order = Order.new
+    @order.items_from_product_ids session[:product_ids]
   end
 
   def show
